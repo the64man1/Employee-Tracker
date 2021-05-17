b1f254b324ad81ff0f0b10f37791603afeb2171b
maxquintanilla <the64man1@yahoo.com>
2021-05-17 15:30:59 -0500

    finished functions to populate arrays of departments/roles/employees, refactored functions with async synax, finished updateEmployeeRole function

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const connection = mysql.createConnection({
     database: 'employee_trackerDB'
 });
 
-const departments = ['Sales', 'Legal', 'Finance', 'Engineering'];
-const roles = ['Business Developer', 'Lawyer', 'Accountant', 'Software Developer'];
-const employees = ['Jack Johnson', 'Sally Smith', 'Bobby Lee', 'Cynthia Burns', 'Tommy Stallings']
+const departments = [];
+const roles = [];
+const employees = [];
 
 const choiceList = [
     {
@@ -26,126 +26,207 @@ const choiceList = [
     }
 ];
 
-const addRolePrompts = [
-    {
-        name: 'title',
-        type: 'input',
-        message: 'What is the title of the role you would like to add?'
-    },
-    {
-        name: 'salary',
-        type: 'input',
-        message: 'What is the salary of the role you are adding?'
-    },
-    {
-        name: 'department',
-        type: 'list',
-        message: 'To which department would you like to assign your new role?',
-        choices: [...departments]
-    }
-]
+const populateDepartments = () => {
+    connection.query(`SELECT name FROM employee_trackerdb.department`, (err, res) => {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            departments.push(res[i].name);
+        }
+    })
+}
 
-const addEmployeePrompts = [
-    {
-        name: 'firstname',
-        type: 'input',
-        message: 'What is the first name of the employee you would like to add?'
-    },
-    {
-        name: 'lastname',
-        type: 'input',
-        message: 'What is the last name of the employee you would like to add?'
-    },
-    {
-        name: 'role',
-        type: 'list',
-        message: 'What is the role of the employee you are adding?',
-        choices: [...roles]
-    },
-    {
-        name: 'manager',
-        type: 'list',
-        message: 'Who is this employees manager?',
-        choices: [...employees, 'Employee does not have a manager']
-    }
-]
+const populateRoles = () => {
+    connection.query(`SELECT title FROM employee_trackerdb.role`, (err, res) => {
+        if (err) throw err;
+        res.forEach((raw) => {
+            const role = raw.title;
+            roles.push(role);
+        })
+    })
+}
+
+const populateEmployees = () => {
+    connection.query(`SELECT first_name, last_name FROM employee_trackerdb.employee`, (err, res) => {
+        if (err) throw err;
+        res.forEach((raw) => {
+            const firstname = raw.first_name;
+            const lastname = raw.last_name;
+            const fullname = `${firstname} ${lastname}`;
+            employees.push(fullname);
+        })
+    })
+}
 
 const viewTable = (table) => {
-    connection.query(`SELECT * FROM ${table}`, (err, res) => {
+     connection.query(`SELECT * FROM ${table}`, (err, res) => {
         if (err) throw err;
         console.table(res);
         mainPrompt(choiceList);
     });
 };
 
-const addDepartment = () => {
-    inquirer.prompt([
+const viewRoles = () => {
+    connection.query(`
+    SELECT role.id, role.title, role.salary, department.name AS 'department'
+    FROM employee_trackerdb.role
+    LEFT JOIN employee_trackerdb.department
+    ON role.department_id = department.id`,
+    (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainPrompt(choiceList);
+    })
+}
+
+// const viewEmployees = () => {
+
+// }
+
+async function addDepartment () {
+    const answer = await inquirer.prompt([
         {
             name: 'department',
             type: 'input',
             message: 'What is the name of the department you would like to add?'
         }
     ])
-    .then((answer) => {
-        if(!answer.department) {
-            console.log('Invalid input, please try again and type in a valid name');
+
+    if(!answer.department) {
+        console.log('Invalid input, please try again and type in a valid name');
+        mainPrompt(choiceList);
+    } else {
+        const department = answer.department;
+        departments.push(department);
+
+        connection.query('INSERT INTO department SET ?', {name: `${department}`}, (err, res) => {
+            if (err) throw err;
+            console.log(`${department} was successfully added`);
             mainPrompt(choiceList);
-        } else {
-            connection.query('INSERT INTO department SET ?', {name: `${answer.department}`}, (err, res) => {
+        });
+    };
+}
+
+async function addRole () {
+
+    const answer = await inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What is the title of the role you would like to add?'
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'What is the salary of the role you are adding?'
+        },
+        {
+            name: 'department',
+            type: 'list',
+            message: 'To which department would you like to assign your new role?',
+            choices: [...departments]
+        }
+    ])
+
+    if(!answer.title || !answer.salary) {
+        console.log('Invalid input, please try again and type in a valid title and/or salary');
+        mainPrompt(choiceList);
+    } else {
+        const role = answer.title;
+        roles.push(role);
+
+        connection.query(`SELECT id FROM employee_trackerdb.department WHERE name='${answer.department}'`, (err, res) => {
+            if (err) throw err;
+            const departmentId = res[0].id;
+            connection.query('INSERT INTO role SET ?', { title: `${answer.title}`, salary: `${answer.salary}`, department_id: `${departmentId}` }, (err, res) => {
                 if (err) throw err;
-                console.log('Your department was successfully added');
+                console.log(`${role} was successfully added`);
                 mainPrompt(choiceList);
             });
-        };
-    });
-};
+        });
+    };
+}
 
-const addRole = () => {
-    inquirer.prompt(addRolePrompts)
-    .then((answer) => {
-        if(!answer.title || !answer.salary) {
-            console.log('Invalid input, please try again and type in a valid title and/or salary');
-            mainPrompt(choiceList);
-        } else {
-            connection.query(`SELECT id FROM employee_trackerdb.department WHERE name='${answer.department}'`, (err, res) => {
+async function addEmployee () {
+
+    let answer = await inquirer.prompt([
+        {
+            name: 'firstname',
+            type: 'input',
+            message: 'What is the first name of the employee you would like to add?'
+        },
+        {
+            name: 'lastname',
+            type: 'input',
+            message: 'What is the last name of the employee you would like to add?'
+        },
+        {
+            name: 'role',
+            type: 'list',
+            message: 'What is the role of the employee you are adding?',
+            choices: [...roles]
+        },
+        {
+            name: 'manager',
+            type: 'list',
+            message: 'Who is this employees manager?',
+            choices: [...employees, 'Employee does not have a manager']
+        }
+    ])
+
+    if(!answer.firstname || !answer.lastname) {
+        console.log('Invalid input, please try again and type in a valid first and/or last name');
+        mainPrompt(choiceList);
+    } else {
+        const employee = `${answer.firstname} ${answer.lastname}`;
+        employees.push(employee);
+
+        const managerFullName = answer.manager.split(' ');
+        const managerFirstName = managerFullName[0];
+        const managerLastName = managerFullName[1];
+        connection.query(`SELECT id FROM employee_trackerdb.role WHERE title='${answer.role}'`, (err, res) => {
+            if (err) throw err;
+            const roleId = res[0].id;
+            connection.query(`SELECT id FROM employee_trackerdb.employee WHERE first_name='${managerFirstName}' AND last_name='${managerLastName}'`, (err, res) => {
                 if (err) throw err;
-                const departmentId = res[0].id;
-                connection.query('INSERT INTO role SET ?', { title: `${answer.title}`, salary: `${answer.salary}`, department_id: `${departmentId}` }, (err, res) => {
+                const managerId = res[0].id;
+                connection.query(`INSERT INTO employee SET ?`, { first_name: `${answer.firstname}`, last_name: `${answer.lastname}`, role_id: `${roleId}`, manager_id: `${managerId}` }, (err, res) => {
                     if (err) throw err;
-                    console.log('Your role was successfully added');
+                    console.log(`${employee} was succesfully added`);
                     mainPrompt(choiceList);
                 });
             });
-        };
-    });
-};
+        });
+    };
+}
 
-const addEmployee = () => {
-    inquirer.prompt(addEmployeePrompts)
-    .then((answer) => {
-        if(!answer.firstname || !answer.lastname) {
-            console.log('Invalid input, please try again and type in a valid first and/or last name');
+async function updateEmployeeRole () {
+    let answer = await inquirer.prompt([
+        {
+            name: 'name',
+            type: 'list',
+            message: 'What is the name of the employee whose role you wish to update?',
+            choices: [...employees]
+        },
+        {
+            name: 'role',
+            type: 'list',
+            message:'Which role do you wish to assign to this employee?',
+            choices: [...roles]
+        }
+    ])
+    const fullName = answer.name.split(' ');
+    const firstName = fullName[0];
+    const lastName = fullName[1];
+    connection.query(`SELECT id FROM employee_trackerdb.role WHERE title='${answer.role}'`, (err, res) => {
+        if (err) throw err;
+        const roleId = res[0].id;
+        connection.query(`UPDATE employee_trackerdb.employee SET role_id='${roleId}' WHERE first_name='${firstName}' AND last_name='${lastName}'`, (err, res) => {
+            if (err) throw err;
+            console.log(`${answer.name}'s role was changed to ${answer.role}`);
             mainPrompt(choiceList);
-        } else {
-            const managerFullName = answer.manager.split(' ');
-            const managerFirstName = managerFullName[0];
-            const managerLastName = managerFullName[1];
-            connection.query(`SELECT id FROM employee_trackerdb.role WHERE title='${answer.role}'`, (err, res) => {
-                if (err) throw err;
-                const roleId = res[0].id;
-                connection.query(`SELECT id FROM employee_trackerdb.employee WHERE first_name='${managerFirstName}' AND last_name='${managerLastName}'`, (err, res) => {
-                    if (err) throw err;
-                    const managerId = res[0].id;
-                    connection.query(`INSERT INTO employee SET ?`, { first_name: `${answer.firstname}`, last_name: `${answer.lastname}`, role_id: `${roleId}`, manager_id: `${managerId}` }, (err, res) => {
-                        if (err) throw err;
-                        console.log('The employee was succesfully added');
-                        mainPrompt(choiceList);
-                    });
-                });
-            });
-        };
-    });
-};
+        })
+    })
+}
 
 const mainPrompt = (list) => {
     inquirer.prompt(list)
@@ -160,7 +241,7 @@ const handleChoice = (answer) => {
             viewTable('department');
             break;
         case 'View Roles':
-            viewTable('role');
+            viewRoles();
             break;
         case 'View Employees':
             viewTable('employee');
@@ -189,4 +270,7 @@ const startPrompt = () => {
     mainPrompt(choiceList);
 };
 
+populateDepartments();
+populateRoles();
+populateEmployees();
 startPrompt();
