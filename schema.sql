c67c24070ae0dda86362ec6f26c4fa8972edc7ac
Antonio Quintanilla <the64man1@yahoo.com>
2021-05-22 17:56:23 -0500

    Update schema.sql
    Added missing semicolons

--- schema.sql
+++ schema.sql
@@ -7,14 +7,14 @@ USE employee_trackerDB;
 CREATE TABLE department (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL
-)
+);
 
 CREATE TABLE employee (
     id INT PRIMARY KEY AUTO_INCREMENT,
